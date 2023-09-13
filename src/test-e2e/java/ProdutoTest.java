704a4b5eb66b9c19472ddc5c42e8f9a21ff3d51a
Gabriel Kleiman <kleim4n@gmail.com>
2023-09-13 09:06:50 -0300

    testE2e: CRUD produto

--- src/test-e2e/java/ProdutoTest.java
+++ src/test-e2e/java/ProdutoTest.java
@@ -1,10 +1,12 @@
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Date;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.apache.http.HttpStatus.SC_NO_CONTENT;
 import static org.apache.http.HttpStatus.SC_OK;
+import static org.hamcrest.Matchers.is;
 
 public class ProdutoTest {
 
@@ -12,7 +14,7 @@ public class ProdutoTest {
     public static final String V1_PRODUTOS_ID = "/api/v1/produtos/{id}";
 
     record ProdutoPost(String descricao, Double preco, Integer estoque) {}
-    static ProdutoPost produtoPost = new ProdutoPost("Produto 1", 10.0, 1);
+    static ProdutoPost produtoPost = new ProdutoPost("Produto %s".formatted(new Date().getTime()), 10.0, 1);
     static ProdutoPost produto2Post = new ProdutoPost("Produto 2", 10.0, 1);
     @Test
     void testCrudProduto() {
@@ -36,7 +38,22 @@ public class ProdutoTest {
                 .get(V1_PRODUTOS_ID)
             .then()
                 .statusCode(SC_OK)
-                .equals(produto2Post);// não funciona
+                .body("descricao", is(produtoPost.descricao()))
+                .body("preco", is(produtoPost.preco().floatValue()))
+                .body("estoque", is(produtoPost.estoque()));
+        given()
+                .baseUri(LOCALHOST)
+                .pathParam("id", id)
+                .pathParam("estoque", 2)
+            .when()
+                .patch("/api/v1/produtos/{id}/estoque/{estoque}")
+            .then()
+                .statusCode(SC_OK)
+                .log().all()
+                .body("descricao", is(produtoPost.descricao()))
+                .body("preco", is(produtoPost.preco().floatValue()))
+                .body("estoque", is(2));
+
         given()
                 .baseUri(LOCALHOST)
                 .pathParam("id", id)
